f272eea7ab9404cab100a79e3802648d4460e8f9
ehudtal <ehud78@gmail.com>
2017-02-15 16:55:06 -0500

    Printer-friendly fixes to kits

--- wp-login.php
+++ wp-login.php
@@ -59,7 +59,6 @@ function login_header( $title = 'Log In', $message = '', $wp_error = '' ) {
 
 	?><!DOCTYPE html>
 	<!--[if IE 8]>
-		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" <?php language_attributes(); ?>>
 	<![endif]-->
 	<!--[if !(IE 8) ]><!-->
 		<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
